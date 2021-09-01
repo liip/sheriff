liip/sheriff

commit bfb5dbaf8ee51e8089e98301b7abeef61778b66d

ISSUE#31 Fixed panic on nil ptr

sheriff.go
@@ -7,7 +7,7 @@ import (
 	"reflect"
 	"strings"
 
-	version "github.com/hashicorp/go-version"
+	"github.com/hashicorp/go-version"
 )
 
 // Options determine which struct fields are being added to the output map.
@@ -216,6 +216,13 @@ func marshalValue(options *Options, v reflect.Value) (interface{}, error) {
 		return val, nil
 	}
 	k := v.Kind()
+
+	switch k {
+	case reflect.Interface, reflect.Map, reflect.Ptr, reflect.Slice:
+		if v.IsNil() {
+			return val, nil
+		}
+	}
 
 	if k == reflect.Ptr {
 		v = v.Elem()

sheriff_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 	"time"
 
-	version "github.com/hashicorp/go-version"
+	"github.com/hashicorp/go-version"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -712,4 +712,24 @@ func TestMarshal_EmptyInterface(t *testing.T) {
 
 	_, err := Marshal(o, v)
 	assert.NoError(t, err)
+}
+
+func TestMarshal_BooleanPtrMap(t *testing.T) {
+	tru := true
+
+	toMarshal := map[string]*bool{
+		"example": &tru,
+		"another": nil,
+	}
+
+	marshalMap, err := Marshal(&Options{}, toMarshal)
+	assert.NoError(t, err)
+
+	marshal, err := json.Marshal(marshalMap)
+	assert.NoError(t, err)
+
+	expect, err := json.Marshal(toMarshal)
+	assert.NoError(t, err)
+
+	assert.Equal(t, string(marshal), string(expect))
 }

commit 0b23eeebffb1c2990dcdd4987b8cf620fd29ce66

Fix slice regression caused by #32
Slices that are nil are empty and valid.
The change introduced by #32 which fixes #31 has changed how
nil slices are marshaled (`null` instead of `[]`).

Fix this regression by removing slices from the check.

sheriff.go
@@ -218,7 +218,7 @@ func marshalValue(options *Options, v reflect.Value) (interface{}, error) {
 	k := v.Kind()
 
 	switch k {
-	case reflect.Interface, reflect.Map, reflect.Ptr, reflect.Slice:
+	case reflect.Interface, reflect.Map, reflect.Ptr:
 		if v.IsNil() {
 			return val, nil
 		}

sheriff_test.go
@@ -630,8 +630,8 @@ func TestMarshal_ArrayOfInterfaceable(t *testing.T) {
 type TestInlineStruct struct {
 	// explicitly testing unexported fields
 	// golangci-lint complains about it and that's ok to ignore.
-	tableName        struct{ Test string } `json:"-"`  //nolint
-	tableNameWithTag struct{ Test string } `json:"foo"`  //nolint
+	tableName        struct{ Test string } `json:"-"`   //nolint
+	tableNameWithTag struct{ Test string } `json:"foo"` //nolint
 
 	Field  string  `json:"field"`
 	Field2 *string `json:"field2"`
@@ -732,4 +732,18 @@ func TestMarshal_BooleanPtrMap(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.Equal(t, string(marshal), string(expect))
+}
+
+func TestMarshal_NilSlice(t *testing.T) {
+	var stringSlice []string // nil slice
+
+	marshalSlice, err := Marshal(&Options{}, stringSlice)
+	assert.NoError(t, err)
+
+	jsonResult, err := json.Marshal(marshalSlice)
+	assert.NoError(t, err)
+
+	expect := "[]"
+
+	assert.Equal(t, expect, string(jsonResult))
 }